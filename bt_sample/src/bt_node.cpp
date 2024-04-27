182198512fc8739ca3b978e6d7763f27d95d063e
shkwon98 <seonghyeon.kwon@avikus.ai>
2024-04-27 22:15:37 +0900

    Refactor BTNode class and main function

--- bt_sample/src/bt_node.cpp
+++ bt_sample/src/bt_node.cpp
@@ -21,89 +21,43 @@
 
 using namespace std::chrono_literals;
 
-// Behavior Tree 실행을 제어하기 위한 전역 atomic 변수 정의
-std::atomic_bool switchActive{ true };
-
-// 파일 경로 설정
-const std::string default_bt_xml_file = ament_index_cpp::get_package_share_directory("bt_sample") + "/config/main_bt.xml";
-const std::string default_bt_log_file = ament_index_cpp::get_package_share_directory("bt_sample") + "/log/bt_trace.btlog";
-
-class BTNode : public rclcpp::Node
+class ROS2Node : public rclcpp::Node
 {
 public:
-    BTNode()
-        : Node("bt_node")
+    ROS2Node(std::string node_name)
+        : rclcpp::Node(node_name)
     {
-        this->declare_parameter<std::string>("tree_xml_file", default_bt_xml_file);
-        tree_xml_file_ = this->get_parameter("tree_xml_file").as_string();
-        this->declare_parameter<std::string>("tree_log_file", default_bt_log_file);
-        tree_log_file_ = this->get_parameter("tree_log_file").as_string();
     }
-
-    void execute()
-    {
-        create_behavior_tree();
-
-        // const auto timer_period = 500ms;
-        // timer_ = this->create_wall_timer(timer_period, std::bind(&BTNode::update_behavior_tree, this));
-        bt_thread_ = std::thread([this]() { tree_.tickWhileRunning(); });
-        RCLCPP_INFO(this->get_logger(), "Behavior Tree is running...");
-    }
-
-    void create_behavior_tree()
-    {
-        BT::BehaviorTreeFactory factory;
-        factory.registerNodeType<TopicDetected>("TopicDetected", shared_from_this());
-        factory.registerNodeType<TopicPubConditionNode>("TopicPubConditionNode", shared_from_this());
-        factory.registerNodeType<TopicDetectedSyncAction>("TopicDetectedSyncAction", shared_from_this());
-
-        auto blackboard = BT::Blackboard::create();
-        tree_ = factory.createTreeFromFile(tree_xml_file_, blackboard);
-
-        // Set up tree logging to monitor the tree in Groot2.
-        publisher_ptr_ = std::make_unique<BT::Groot2Publisher>(tree_, 5555);
-        // Logger의 생명주기를 관리하기 위해 클래스 멤버로 선언
-        logger_file_ = std::make_unique<BT::FileLogger2>(tree_, tree_log_file_);
-        logger_cout_ = std::make_unique<BT::StdCoutLogger>(tree_);
-    }
-
-    void update_behavior_tree()
-    {
-        BT::NodeStatus tree_status = tree_.tickOnce();
-        if (tree_status != BT::NodeStatus::RUNNING)
-        {
-            RCLCPP_INFO(this->get_logger(), "Behavior Tree finished with status: %s",
-                        tree_status == BT::NodeStatus::SUCCESS ? "SUCCESS" : "FAILURE");
-            timer_->cancel();
-        }
-    }
-
-    // Configuration parameters.
-    std::string tree_xml_file_;
-    std::string tree_log_file_;
-
-    // ROS and BehaviorTree.CPP variables.
-    rclcpp::TimerBase::SharedPtr timer_;
-    BT::Tree tree_;
-    std::thread bt_thread_;
-    std::unique_ptr<BT::Groot2Publisher> publisher_ptr_;
-    std::unique_ptr<BT::FileLogger2> logger_file_;
-    std::unique_ptr<BT::StdCoutLogger> logger_cout_;
 };
 
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
+    auto ros2_node = std::make_shared<ROS2Node>("bt_node");
 
-    auto node = std::make_shared<BTNode>();
-    node->execute();
+    auto factory = BT::BehaviorTreeFactory();
+    factory.registerNodeType<TopicDetected>("TopicDetected", ros2_node);
+    factory.registerNodeType<TopicPubConditionNode>("TopicPubConditionNode", ros2_node, 2s);
+    factory.registerNodeType<TopicDetectedSyncAction>("TopicDetectedSyncAction", ros2_node);
 
-    while (rclcpp::ok())
-    {
-        rclcpp::spin_some(node);
-    }
+    const auto default_bt_xml_file = ament_index_cpp::get_package_share_directory("bt_sample") + "/config/main_bt.xml";
+    const auto default_bt_log_file = ament_index_cpp::get_package_share_directory("bt_sample") + "/log/bt_trace.btlog";
 
-    RCLCPP_INFO(node->get_logger(), "Shutting down.");
-    rclcpp::shutdown();
+    auto blackboard = BT::Blackboard::create();
+    auto tree = factory.createTreeFromFile(default_bt_xml_file, blackboard);
+
+    auto groot2_publisher = std::make_unique<BT::Groot2Publisher>(tree, 5555);
+    auto bt_file_logger = std::make_unique<BT::FileLogger2>(tree, default_bt_log_file);
+    auto bt_cout_logger = std::make_unique<BT::StdCoutLogger>(tree);
+
+    auto bt_executer = std::thread([&]() {
+        tree.tickWhileRunning();
+        rclcpp::shutdown();
+    });
+
+    rclcpp::spin(ros2_node);
+    bt_executer.join();
+
+    RCLCPP_INFO(ros2_node->get_logger(), "Shutting down.");
     return 0;
 }
